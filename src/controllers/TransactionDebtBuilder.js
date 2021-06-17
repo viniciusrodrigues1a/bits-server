877675a5259eb19077eccf3b54fb676da42e3d98
Julin ddd 011 <wellingtonjr53@outlook.com.br>
2021-06-16 21:23:15 -0300

    removed archives deleted in repository

--- src/controllers/TransactionDebtBuilder.js
+++
@@ -1,9 +0,0 @@
-export default class TransactionDebtBuilder {
-  constructor({ database }) {
-    this.database = database;
-  }
-
-  store(amount, incoming, categoryId, walletId, description) {}
-
-  removeAmountWallet() {}
-}
