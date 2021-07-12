d97bb83068d063612b073d75fcc67cef4804af3b
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2021-07-12 07:01:58 -0300

    add test: return 404 if WalletNotFoundError is thrown by use-case

--- src/modules/transactions/presentation/controllers/CreateTransactionController.test.ts
+++ src/modules/transactions/presentation/controllers/CreateTransactionController.test.ts
@@ -1,15 +1,34 @@
 import { mock } from 'jest-mock-extended';
-import { ICreateTransactionUseCase } from 'modules/transactions/domain/use-cases/ICreateTransactionUseCase';
+import { WalletNotFoundError } from '../../domain/use-cases/errors';
+import { ICreateTransactionUseCase } from '../..//domain/use-cases/ICreateTransactionUseCase';
 import { statusCodes } from '../helpers';
 import { CreateTransactionController } from './CreateTransactionController';
 
+function makeSut() {
+  const createTransactionUseCaseMock = mock<ICreateTransactionUseCase>();
+  const sut = new CreateTransactionController(createTransactionUseCaseMock);
+
+  return { sut, createTransactionUseCaseMock };
+}
+
 describe('CreateTransactionController', () => {
   it('should return an okResponse', async () => {
-    const createTransactionUseCaseMock = mock<ICreateTransactionUseCase>();
-    const sut = new CreateTransactionController(createTransactionUseCaseMock);
+    const { sut } = makeSut();
 
     const response = await sut.handleRequest({ body: {} });
 
     expect(response.statusCode).toBe(statusCodes.ok);
+  });
+
+  it('should return notFound if use-case throws WalletNotFoundError', async () => {
+    const { sut, createTransactionUseCaseMock } = makeSut();
+
+    createTransactionUseCaseMock.create.mockImplementationOnce(() => {
+      throw new WalletNotFoundError();
+    });
+
+    const response = await sut.handleRequest({ body: {} });
+
+    expect(response.statusCode).toBe(statusCodes.notFound);
   });
 });
