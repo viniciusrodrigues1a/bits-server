69e2dfd467f9a04074e4361fa5332e088bb419fe
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2021-07-15 09:14:07 -0300

    refactor: rename usecase factory

--- src/main/factories/useCases/index.ts
+++ src/main/factories/useCases/index.ts
@@ -1,3 +1,3 @@
 export * from './CreateTransactionUseCaseFactory';
 export * from './DeleteTransactionUseCaseFactory';
-export * from './ValidateAuthenticationUseCase';
+export * from './ValidateAuthenticationUseCaseFactory';
